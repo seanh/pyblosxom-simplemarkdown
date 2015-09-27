35f354740b33cd595c5035c579f26184ba5d1299
Sean Hammond <git@seanh.cc>
2015-09-27 20:59:06 +0100

    Simplify installation

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.1',
+    version='0.0.2',
 
     description="A Pyblosxom entryparser plugin that uses Markdown syntax and parses the whole file as the entry body",
     long_description=long_description,
